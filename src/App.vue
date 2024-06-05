033b938c87afcbcc3f541506406240cb07873308
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-05 19:13:54 +0200

    [UPD] CardsComponent

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,22 @@ import { store } from "./store";
 import axios from "axios";
 
 import SearchComponent from './components/SearchComponent.vue';
+import CardsComponent from "./components/CardsComponent.vue";
 
 export default {
   name: "MyApp",
   components: {
-    SearchComponent
+    SearchComponent,
+    CardsComponent
   },
 
   data() {
     return {
-      store,
-      langs: {
-        en: ["en", "england"],
-        it: ["it", "italy"],
-        es: ["es", "spain"],
-        ja: ["ja", "japan"],
-        fr: ["fr", "france"],
-        de: ["de", "germany"]
-      }
+      store
     }
   },
 
   methods: {
-
-    findLang(language) {
-      for (let key in this.langs) {
-        if (this.langs[key][0] === language) {
-          return this.langs[key];
-        }
-      }
-    },
 
     search() {
 
@@ -65,63 +51,29 @@ export default {
 <template>
   <h1>Boolflix</h1>
   <SearchComponent @search="search" />
-
   <div class="container" v-show="store.movieResults.length">
     <h2>Movies</h2>
     <ul>
-      <li v-for="result in store.movieResults">
-        <p>{{ result.title }}</p>
-        <p>{{ result.original_title }}</p>
-        <div>
-          <img v-if="findLang(result.original_language)" :src="`/img/${findLang(result.original_language)[1]}_flag.png`"
-            :alt="result.original_language">
-          <p v-else>{{ result.original_language }}</p>
-        </div>
-        <p>{{ result.vote_average }}</p>
-      </li>
+      <CardsComponent v-for="result in store.movieResults" :title="result.title" :original_title="result.original_title"
+        :language="result.original_language" :vote="result.vote_average" />
     </ul>
   </div>
-
   <div class="container" v-show="store.tvResults.length">
     <h2>Tv series</h2>
     <ul>
-      <li v-for="result in store.tvResults">
-        <p>{{ result.name }}</p>
-        <p>{{ result.original_name }}</p>
-        <div>
-          <img v-if="findLang(result.original_language)" :src="`/img/${findLang(result.original_language)[1]}_flag.png`"
-            :alt="result.original_language">
-          <p v-else>{{ result.original_language }}</p>
-        </div>
-        <p>{{ result.vote_average }}</p>
-      </li>
+      <CardsComponent v-for="result in store.tvResults" :title="result.name" :original_title="result.original_name"
+        :language="result.original_language" :vote="result.vote_average" />
     </ul>
   </div>
 </template>
 
 <style scoped>
+.container {
+  margin: 30px 20px;
+}
+
 ul {
   display: flex;
   flex-wrap: wrap;
-}
-
-li {
-  width: calc(25% - 40px);
-  margin: 20px 15px 20px 0;
-  border: 1px solid black;
-  padding-left: 5px;
-}
-
-p {
-  padding: 5px 0;
-}
-
-img {
-  width: 50px;
-  height: 50px;
-}
-
-.container {
-  margin: 30px 20px;
 }
 </style>
---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+    name: "CardsComponent",
+    props: ["title", "original_title", "language", "vote"],
+    data() {
+        return {
+            langs: {
+                en: ["en", "england"],
+                it: ["it", "italy"],
+                es: ["es", "spain"],
+                ja: ["ja", "japan"],
+                fr: ["fr", "france"],
+                de: ["de", "germany"]
+            }
+        }
+    },
+
+    methods: {
+        findLang(language) {
+            for (let key in this.langs) {
+                if (this.langs[key][0] === language) {
+                    return this.langs[key];
+                }
+            }
+        },
+    }
+}
+
+</script>
+
+<template>
+    <li>
+        <p>{{ title }}</p>
+        <p>{{ original_title }}</p>
+        <div>
+            <img v-if="findLang(language)" :src="`/img/${findLang(language)[1]}_flag.png`" :alt="language">
+            <p v-else>{{ language }}</p>
+        </div>
+        <p>{{ vote }}</p>
+    </li>
+</template>
+
+<style scoped>
+li {
+    width: calc(25% - 40px);
+    margin: 20px 15px 20px 0;
+    border: 1px solid black;
+    padding-left: 5px;
+}
+
+p {
+    padding: 5px 0;
+}
+
+img {
+    width: 50px;
+    height: 50px;
+}
+</style>
